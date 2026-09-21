f73f3cb397c7994fe11d2aa246a5a6fca485e830
Ubuntu <ubuntu@localhost.localdomain>
2026-09-21 18:23:35 +0800

    fix: bump IDB version to 2 for missing stores

--- lib/chatFiles.ts
+++ lib/chatFiles.ts
@@ -1,6 +1,6 @@
 const DB_NAME = "runwithme_chat_files_db";
 const STORE_NAME = "chat_files";
-const DB_VERSION = 1;
+const DB_VERSION = 2;
 
 function openDatabase(): Promise<IDBDatabase> {
   return new Promise((resolve, reject) => {
--- lib/stickerFiles.ts
+++ lib/stickerFiles.ts
@@ -3,7 +3,7 @@ const STORE_NAME = "sticker_files";
 
 function openDatabase(): Promise<IDBDatabase> {
   return new Promise((resolve, reject) => {
-    const request = indexedDB.open(DB_NAME, 1);
+    const request = indexedDB.open(DB_NAME, 2);
 
     request.onupgradeneeded = () => {
       const db = request.result;
